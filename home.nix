6fb209e7854f6f6ccf42552167bbb55ba42b9010
Ben Pearson <blpearson44@icloud.com>
2023-10-04 14:10:28 -0700

    move home -> xdg

--- home.nix
+++ home.nix
@@ -10,8 +10,10 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  xdg.configFile = {
     "kitty".source = ./.config/kitty;
+  };
+  home.file = {
   };
 
   programs = {
